fb0a2bda68e20db0791e07d3673bd0308ac06f3d
André Rafael <andrerrck@outlook.com>
2024-09-12 18:11:21 -0300

    Layout + first page initial design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,19 @@
 <script>
-  import '../app.css';
+  import "../app.css";
 </script>
 
-<header>
-  <h1>Header</h1>
-</header>
+<div class="flex flex-col min-h-screen bg-gray-900 text-gray-300">
+  <main class="flex-grow p-4">
+    <slot />
+  </main>
 
-<main>
-  <slot /> <!-- This is where the child route content will be rendered -->
-</main>
-
-<footer>
-  <p>Footer</p>
-</footer>
+  <footer class="bg-gray-850 text-gray-400 p-4 text-center flex-shrink-0">
+    <p>
+      Made with 🔥 by <a
+        href="https://github.com/andrerrazambuja"
+        class="underline text-gray-300 hover:text-purple-400"
+        >@andrerrazambuja</a
+      >
+    </p>
+  </footer>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,43 @@
-<main>
-    <div class="bg-blue-500 text-white p-4 rounded-lg">
-    Teste Tailwind CSS
-    </div>
-</main>
+<script>
+  const roadmaps = [
+    {
+      name: "Front-End Developer",
+      description:
+        "Learn the skills necessary to build beautiful and responsive user interfaces using HTML, CSS, and JavaScript.",
+    },
+    {
+      name: "Back-End Developer",
+      description:
+        "Focus on building APIs, working with databases, and server-side logic with languages like Node.js, Python, or Java.",
+    },
+    {
+      name: "Full-Stack Developer",
+      description:
+        "Become proficient in both front-end and back-end development, enabling you to build complete applications from start to finish.",
+    }
+  ];
+</script>
+
+<div
+  class="flex flex-col items-center justify-start bg-gray-900 text-gray-300 pt-8"
+>
+  <h1
+    class="text-5xl font-bold mb-8 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent"
+  >
+    DevMap
+  </h1>
+
+  <!-- Roadmaps List -->
+  <div
+    class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full px-4 md:px-16 justify-items-center align-items-center"
+  >
+    {#each roadmaps as roadmap}
+      <div
+        class="bg-gray-800 p-6 rounded-lg shadow-lg flex flex-col justify-between w-full md:w-auto"
+      >
+        <h2 class="text-2xl font-bold text-purple-400 mb-2">{roadmap.name}</h2>
+        <p class="text-gray-400">{roadmap.description}</p>
+      </div>
+    {/each}
+  </div>
+</div>
