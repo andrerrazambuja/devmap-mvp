c6d1898b6eeb120d462de79491c5d74d3419d6cf
André Rafael <andrerrck@outlook.com>
2024-09-14 14:50:19 -0300

    begginer roadmap localization

--- src/routes/roadmap/beginner/+page.svelte
+++ src/routes/roadmap/beginner/+page.svelte
@@ -1,29 +1,30 @@
 <script>
-  import RoadmapComponent from '$lib/RoadmapComponent.svelte';
+  import RoadmapComponent from "$lib/RoadmapComponent.svelte";
+  import { t } from "svelte-i18n";
 
-  let events = [
-  { 
-    title: "Aprender Lógica de Programação", 
-    description: "Antes de mergulhar no código, é importante entender como resolver problemas de forma lógica. Estude conceitos como variáveis, loops, e condições." 
-  },
-  { 
-    title: "Familiarize-se com Estruturas de Dados Simples", 
-    description: "Aprenda sobre arrays, listas, e dicionários (ou objetos), que são fundamentais para organizar e processar dados em programação." 
-  },
-  { 
-    title: "Entenda os Conceitos de Algoritmos", 
-    description: "Estude algoritmos comuns, como ordenação e busca, que são amplamente utilizados em soluções de problemas reais no mundo da programação." 
-  },
-  { 
-    title: "Aprenda sobre Desenvolvimento Web Básico", 
-    description: "Familiarize-se com HTML, CSS e JavaScript para entender os fundamentos de como as páginas da web funcionam e são desenvolvidas." 
-  },
-  { 
-    title: "Domine o Controle de Versão com Git", 
-    description: "O Git é uma ferramenta essencial para qualquer programador. Aprenda a versionar seu código e compartilhar projetos no GitHub." 
-  },
-];
-
+  let events;
+  $: events = [
+    {
+      title: $t("roadmaps.beginner.events.logic.title"),
+      description: $t("roadmaps.beginner.events.logic.description"),
+    },
+    {
+      title: $t("roadmaps.beginner.events.dataStructures.title"),
+      description: $t("roadmaps.beginner.events.dataStructures.description"),
+    },
+    {
+      title: $t("roadmaps.beginner.events.algorithms.title"),
+      description: $t("roadmaps.beginner.events.algorithms.description"),
+    },
+    {
+      title: $t("roadmaps.beginner.events.webDevelopment.title"),
+      description: $t("roadmaps.beginner.events.webDevelopment.description"),
+    },
+    {
+      title: $t("roadmaps.beginner.events.git.title"),
+      description: $t("roadmaps.beginner.events.git.description"),
+    },
+  ];
 </script>
 
 <RoadmapComponent {events} />
